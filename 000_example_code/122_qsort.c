fe4e284b3eb8de3221ccaaafe102ee0c7de17b70
Nicholas Gobern <nickgobern@hotmail.com>
2018-03-05 10:03:41 -0500

    Update 122_qsort.c

--- 000_example_code/122_qsort.c
+++ 000_example_code/122_qsort.c
@@ -13,12 +13,17 @@
 int compare_int( const void* a, const void* b ) 
 { 
      if( *(int*)a == *(int*)b ) return 0;
-     return *(int*)a < *(int*)b ? -1 : 1;
+     return *(int*)a < *(int*)b ? -1 : 1;//decending
+//   return *(int*)a > *(int*)b ? -1 : 1; ascending
 }  
 
 int main(void)
 {
     const size_t num_elem = 10;
-    int elements[num_elem] = { 3, 6, 1, 9, 8, 2, 0, 5, 7, 4 };
+    int elements[10] = { 3, 6, 1, 9, 8, 2, 0, 5, 7, 4 };
     qsort( elements, num_elem, sizeof(int), compare_int );
+    for(int n = 0; n < num_elem; n ++)
+       printf("%d ", elements[n]);
+    printf("\n");
+
 }
